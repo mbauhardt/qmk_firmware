98ab0f8e612292ffdb6edfb5f5315fdb6b90485f
Marko Bauhardt <marko.bauhardt@mailbox.org>
2019-04-28 12:47:53 +0200

    added dvorak2 layout to have additional charakters

--- keyboards/keebio/iris/keymaps/mbauhardt/keymap.c
+++ keyboards/keebio/iris/keymaps/mbauhardt/keymap.c
@@ -2,13 +2,17 @@
 
 extern keymap_config_t keymap_config;
 
+// keycodes
+#define KC_XXX		KC_NO // three "XXX" for no operation
+#define KC_DVRK2	MO(1) // switch to layer dvorak2 to have additional characters
 
-#define KC_XXX   KC_NO // three "XXX" for no operation
-
-#define _DVORAK  0
+// layer
+#define _DVORAK		0
+#define _DVORAK_2	1
 
 enum custom_keycodes {
   DVORAK,
+  DVORAK_2,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -24,13 +28,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * +-----+-----+-----+-----+-----+-----+-----.    ,-----+-----+-----+-----+-----+-----+-----+
      * | LCTL|  ;  |  Q  |  J  |  K  |  X  |     /    \     |  B  |  M  |  W  |  V  |  Z  | RCTL|
      * +-----+-----+-----+--+--+-----+-----+----/      \----+-----+-----+--+--+-----+-----+-----+
-     *                      \ LALT| LCMD| SPC  /        \  ENT |     |     /
+     *                      \ LALT| LCMD| SPC  /        \  ENT |     |DVRK2/
      *                       `----+-----+-----'          `-----+-----+----'
      */
      ESC , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,BSPC,
      TAB ,QUOT,COMM, DOT, P  , Y  ,                F  , G  , C  , R  , L  ,SLSH,
      LSFT, A  , O  , E  , U  , I  ,                D  , H  , T  , N  , S  ,RSFT,
      LCTL,SCLN, Q  , J  , K  , X  , XXX,      XXX, B  , M  , W  , V  , Z  ,RCTL,
-                       LALT,LCMD, SPC,         ENT, XXX, XXX
+                       LALT,LCMD, SPC,         ENT, XXX, DVRK2
+  ),
+
+  [_DVORAK_2] = LAYOUT_kc(
+    /*
+     * +-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * |  `  |     |     |     |     |     |                |     |     |     |     |  [  |  ]  |
+     * +-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * |     |     |     |     |     |     |                |     |     |     |     |  =  |  \  |
+     * +-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * |     |     |     |     |     |     |                |     |     |     |     |  -  |     |
+     * +-----+-----+-----+-----+-----+-----+-----.    ,-----+-----+-----+-----+-----+-----+-----+
+     * |     |     |     |     |     |     |     /    \     |     |     |     |     |     |     |
+     * +-----+-----+-----+--+--+-----+-----+----/      \----+-----+-----+--+--+-----+-----+-----+
+     *                      \     |     |      /        \      |     |     /
+     *                       `----+-----+-----'          `-----+-----+----'
+     */
+     GRV , XXX , XXX , XXX , XXX , XXX ,                XXX , XXX , XXX , XXX , LBRC, RBRC,
+     XXX , XXX , XXX , XXX , XXX , XXX ,                XXX , XXX , XXX , XXX , EQL , BSLS,
+     XXX , XXX , XXX , XXX , XXX , XXX ,                XXX , XXX , XXX , XXX , MINS, XXX ,
+     XXX , XXX , XXX , XXX , XXX , XXX , XXX ,    XXX , XXX ,  XXX , XXX , XXX , XXX , XXX ,
+                         LALT ,LCMD , SPC ,          XXX , XXX, XXX
   )
 };
