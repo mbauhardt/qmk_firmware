e835fb1ecb1caf05cb57098a1982c4f6de87ce36
Marko Bauhardt <marko.bauhardt@mailbox.org>
2019-05-02 16:00:07 +0200

    added vim layer

--- keyboards/keebio/iris/keymaps/mbauhardt/keymap.c
+++ keyboards/keebio/iris/keymaps/mbauhardt/keymap.c
@@ -2,20 +2,32 @@
 
 extern keymap_config_t keymap_config;
 
-// keycodes
+// layer keycodes
 #define KC_XXX		KC_NO // three "XXX" for no operation
 #define KC_DVRK2	MO(1) // switch to layer dvorak2 to have additional characters
 #define KC_FN		MO(2) // fn keys, volume keys
+#define KC_VIM		TG(3) // "VIM" mode, this layer is there to navigate in a browser or os related editor which is not vim based
+
+// VIM based shortcuts while working in a OS text editor or browser textfield
+#define KC_ALTL		LALT(KC_LEFT)
+#define KC_ALTR		LALT(KC_RGHT)
+#define KC_CMDL		LCMD(KC_LEFT)
+#define KC_CMDR		LCMD(KC_RGHT)
+#define KC_CMDC		LCMD(KC_C)
+#define KC_CMDX		LCMD(KC_X)
+#define KC_CMDV		LCMD(KC_V)
 
 // layer
 #define _DVORAK		0
 #define _DVORAK_2	1
 #define _FN		2
+#define _VIM		3
 
 enum custom_keycodes {
   DVORAK,
   DVORAK_2,
-  FN
+  FN,
+  VIM
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -31,14 +43,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * +-----+-----+-----+-----+-----+-----+-----.    ,-----+-----+-----+-----+-----+-----+-----+
      * | LCTL|  ;  |  Q  |  J  |  K  |  X  | FN  /    \  FN |  B  |  M  |  W  |  V  |  Z  | RCTL|
      * +-----+-----+-----+--+--+-----+-----+----/      \----+-----+-----+--+--+-----+-----+-----+
-     *                      \ LALT| LCMD| SPC  /        \  ENT |     |DVRK2/
+     *                      \ LALT| LCMD| SPC  /        \  ENT | VIM |DVRK2/
      *                       `----+-----+-----'          `-----+-----+----'
      */
      ESC , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,BSPC,
      TAB ,QUOT,COMM, DOT, P  , Y  ,                F  , G  , C  , R  , L  ,SLSH,
      LSFT, A  , O  , E  , U  , I  ,                D  , H  , T  , N  , S  ,RSFT,
      LCTL,SCLN, Q  , J  , K  , X  , FN,      FN, B  , M  , W  , V  , Z  ,RCTL,
-                       LALT,LCMD, SPC,         ENT, XXX, DVRK2
+                       LALT,LCMD, SPC,         ENT, VIM, DVRK2
   ),
 
   [_DVORAK_2] = LAYOUT_kc(
@@ -81,5 +93,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      LSFT, XXX , XXX , XXX , XXX , XXX ,                XXX , XXX , XXX , XXX , XXX , RSFT,
      XXX , XXX , XXX , XXX , XXX , XXX , XXX ,    XXX , XXX ,  XXX , XXX , XXX , XXX , XXX ,
                          LALT ,LCMD , XXX ,          XXX , VOLU, VOLD
-  )
+  ),
+
+  [_VIM] = LAYOUT_kc(
+    /*
+     * +-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * |     |     |     |     | HOME|     |                |     |     |     |     |  END| BSPC|
+     * +-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * |     |     |     |     |PASTE| COPY|                |     |     |     |     | RGHT|     |
+     * +-----+-----+-----+-----+-----+-----+                +-----+-----+-----+-----+-----+-----+
+     * | LSFT|     |     |     |     |     |                | CUT | LEFT|     |     |     | RSFT|
+     * +-----+-----+-----+-----+-----+-----+-----.    ,-----+-----+-----+-----+-----+-----+-----+
+     * |     |     |     | DOWN|  UP | DEL |     /    \     |  BW |     |  FW |     |     |     |
+     * +-----+-----+-----+--+--+-----+-----+----/      \----+-----+-----+--+--+-----+-----+-----+
+     *                      \     |     |      /        \  ENT |     |     /
+     *                       `----+-----+-----'          `-----+-----+----'
+     */
+     XXX , XXX , XXX , XXX , CMDR , XXX ,                XXX , XXX , XXX , XXX ,CMDL, BSPC,
+     XXX , XXX , XXX , XXX , CMDV, CMDC,                XXX , XXX , XXX , XXX , RGHT, XXX ,
+     LSFT, XXX , XXX , XXX , XXX , XXX ,                CMDX, LEFT, XXX , XXX , XXX , RSFT,
+     XXX , XXX , XXX , DOWN,  UP , DEL , XXX ,    XXX , ALTL, XXX , ALTR, XXX , XXX , XXX ,
+                         XXX , XXX, XXX ,          ENT , VIM, XXX
+  ),
 };
